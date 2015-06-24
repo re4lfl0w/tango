992dc49188159a5c9d7c19c0eaafa5dab4c4566e
re4lfl0w <re4lfl0w@gmail.com>
2015-06-25 05:43:49 +0900

    Add 5. Templates and Static Media

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Rango says hey there world!')
+    context_dict = {'boldmessage': 'I am bold font from the context'}
+    return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -7,3 +9,14 @@ urlpatterns = patterns('',
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+                            (r'^media/(?P<path>.*)',
+                             'serve',
+                             {'document_root': settings.MEDIA_ROOT}),
+                            )
