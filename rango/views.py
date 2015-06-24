9ebcb6e4586490447c3cdf940b6b0831ef0345e4
re4lfl0w <re4lfl0w@gmail.com>
2015-06-25 02:29:30 +0900

    Complete Exercises about page.

--- rango/views.py
+++ rango/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('Rango says hey there world!')
+
+
+def about(request):
+    return HttpResponse('Rango says here is the about page. <br />'
+    '<a href="/rango/about">About</a> <br />'
+    '<a href="/rango/">Index</a>')
