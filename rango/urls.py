8ec5bf236c47cf2131357a8a8e6becc870441397
re4lfl0w <re4lfl0w@gmail.com>
2015-06-25 20:15:05 +0900

    Add 7. Models, Templates and Views

--- rango/urls.py
+++ rango/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
-
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category,
+        name='category'),
+    # url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),  # New!
 )
