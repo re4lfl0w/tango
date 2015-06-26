3e784c79160c74a0a6b45c8662ab6dbe456af192
re4lfl0w <re4lfl0w@gmail.com>
2015-06-26 22:13:26 +0900

    Complete all other existing template pages within Rango's repertoire to extend from the base.html

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    url(r'^rango/', include('rango.urls')),
+    url(r'^rango/', include('rango.urls', namespace='rango')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
