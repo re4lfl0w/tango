2195b817e410ab1bfc7edd0a9b9c42eceb93fa8c
re4lfl0w <re4lfl0w@gmail.com>
2015-06-29 06:10:06 +0900

    Add 19. AJAX in Django with JQuery. Exciting with jQuery

--- rango/views.py
+++ rango/views.py
@@ -270,6 +270,44 @@ def track_url(request):
 
 
 
+@login_required
+def like_category(request):
+    cat_id = None
+    if request.method == 'GET':
+        cat_id = request.GET['category_id']
+
+    if cat_id:
+        cat = get_object_or_404(Category, id=int(cat_id))
+        if cat:
+            likes = cat.likes + 1
+            cat.likes = likes
+            cat.save()
+    return HttpResponse(likes)
+
+
+def get_category_list(max_results=0, starts_with=''):
+    cat_list = []
+    if starts_with:
+        cat_list = Category.objects.filter(name__istartswith=starts_with)
+
+    if max_results > 0:
+        if len(cat_list) > max_results:
+            cat_list = cat_list[:max_results]
+
+    return cat_list
+
+
+def suggest_category(request):
+    cat_list = []
+    starts_with = ''
+    if request.method == 'GET':
+        starts_with = request.GET['suggestion']
+
+    cat_list = get_category_list(8, starts_with)
+    return render(request, 'rango/category_list.html',
+        {'cat_list': cat_list})
+
+
 # def test_cookie(request):
 #     if 'id' in request.COOKIES:
 #         cookie_id = request.COOKIES['id']
