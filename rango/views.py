e1d5b30ddf06c414c90f1fdff885537ab73270fc
re4lfl0w <re4lfl0w@gmail.com>
2015-06-29 07:15:53 +0900

    Complete no strings in find a category back to print all Categories

--- rango/views.py
+++ rango/views.py
@@ -302,8 +302,10 @@ def suggest_category(request):
     starts_with = ''
     if request.method == 'GET':
         starts_with = request.GET['suggestion']
+        cat_list = get_category_list(8, starts_with)
+        if starts_with == '':
+            cat_list = Category.objects.all()
 
-    cat_list = get_category_list(8, starts_with)
     return render(request, 'rango/category_list.html',
         {'cat_list': cat_list})
 
