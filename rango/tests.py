2c9f435a5a357d40f219fc5a19a5b57e4b231d44
re4lfl0w <re4lfl0w@gmail.com>
2015-06-29 12:23:22 +0900

    Add 20. Automated Testing

--- rango/tests.py
+++ rango/tests.py
@@ -1,3 +1,57 @@
+from datetime import datetime
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+import pytz
+from rango.models import Category, Page
 
-# Create your tests here.
+
+class CategoryMethodTests(TestCase):
+    def test_ensure_views_are_positive(self):
+        cat = Category(name='test', views=1, likes=0)
+        cat.save()
+        self.assertEqual((cat.views >= 0), True)
+
+    def test_slug_line_creation(self):
+        cat = Category(name='Random Category String')
+        cat.save()
+        self.assertEqual(cat.slug, 'random-category-string')
+
+    def test_index_view_with_no_categories(self):
+        response = self.client.get(reverse('rango:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'There are no categories present.')
+        self.assertQuerysetEqual(response.context['categories'], [])
+
+
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
+
+
+class IndexVIewTests(TestCase):
+    def test_index_view_with_categories(self):
+        add_cat('test', 1, 1)
+        add_cat('temp', 1, 1)
+        add_cat('tmp', 1, 1)
+        add_cat('tmp test temp', 1, 1)
+
+        response = self.client.get(reverse('rango:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'tmp test temp')
+
+        num_cats = len(response.context['categories'])
+        self.assertEqual(num_cats, 4)
+
+
+def add_page(category, title, url, views, first_visit, last_visit):
+    c = Category.objects.get_or_create(name=category)[0]
+    p = Page.objects.get_or_create(category=c,
+                                   title=title,
+                                   url=url,
+                                   views=views,
+                                   first_visit=first_visit,
+                                   last_visit=last_visit)[0]
+    return p
